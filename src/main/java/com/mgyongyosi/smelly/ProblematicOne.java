22e6c96ac217ecf52de8572e6cf7b78655dd752a
mgyongyosi <gymisi@gmail.com>
2017-07-06 13:11:33 +0200

    Initial commit

---
+++ src/main/java/com/mgyongyosi/smelly/ProblematicOne.java
@@ -0,0 +1,16 @@
+package com.mgyongyosi.smelly;
+
+/**
+ * Created by mgyongyosi on 2017. 07. 06..
+ */
+public class ProblematicOne {
+    String fieldOne;
+    String fieldTwo;
+
+    public boolean doSomething() {
+        if(fieldOne.toUpperCase().equals(fieldTwo)) {
+            return true;
+        }
+
+    }
+}
